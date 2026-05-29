84aa59bc9cf00987a12b24cd0dff93ece3d80546
Tomas Brizuela <tomas.brizuela.cavallin@gmail.com>
2026-05-29 17:19:15 -0300

    refactor: streamline RutaProtegida component; improve authentication check and code readability

--- e-commerce-front/src/routes/RutaProtegida.jsx
+++ e-commerce-front/src/routes/RutaProtegida.jsx
@@ -1,13 +1,25 @@
 import { Navigate } from 'react-router-dom';
-import { esAdmin }  from '../utils/auth';
+import { esAdmin } from '../utils/auth';
+// import { useAuth } from '../features/auth/context/AuthContext';
 
-function RutaProtegida({ children, soloAdmin = false }) {
+const RutaProtegida = ({ children, soloAdmin = false }) => {
+  //traigo del contexto (o información global) los datos del usuario, si está autenticado, sus datos,
+  // const { isAuthenticated, loading } = useAuth();
+
+  // if (loading) {
+  //   return <div>Cargando...</div>;
+  // }
+
+  //hardcodeado provisoriamente con el token de localStorage, luego mediante jwt en java veremos si está logueado
+  //para ver un ejemplo AuthContext,
   const token = localStorage.getItem('token');
+  const isAuthenticated = !!token;
 
-  // Sin token: redirige al login
-  if (!token) return <Navigate to="/login" replace />;
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
 
-  // Requiere admin pero no lo es: muestra error
+  // Si la ruta requiere admin pero el usuario no lo es, mostramos un error
   if (soloAdmin && !esAdmin()) {
     return (
       <div style={{ padding: '2rem', color: 'red' }}>
@@ -17,6 +29,6 @@ function RutaProtegida({ children, soloAdmin = false }) {
   }
 
   return children;
-}
+};
 
 export default RutaProtegida;
